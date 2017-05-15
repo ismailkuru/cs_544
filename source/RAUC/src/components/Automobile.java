b07663ba154a8de8173691f7851afadde47c5d55
Ismail <ismailkuru>
2017-05-14 23:07:47 -0400

    simplifying main

--- source/RAUC/src/components/Automobile.java
+++ source/RAUC/src/components/Automobile.java
@@ -1,6 +1,7 @@
 package components;
 
 public class Automobile {
-
+//[TODO] User , automobile etc. need to be added. A random autonmobile and component generator
+// might be implemented ....
 	
 }
